7ec4da915279dbf07e3819a37b47a48efc07de0a
DIogo <diogo.souza@olxbr.com>
2018-12-26 21:03:29 +0000

    Demo cljs

--- src/client/main.cljs
+++ src/client/main.cljs
@@ -1,5 +1,15 @@
 (ns client.main)
 
-(println "hi")
+(defn create-canvas
+  [] (.createElement js/document "canvas"))
+
+(defn append-canvas
+  [] (-> js/document
+         (.-body)
+         (.appendChild (create-canvas))))
+
+(defn -main
+  []  (set! (.-onload js/window)
+        (fn [_] (append-canvas))))
 
 
