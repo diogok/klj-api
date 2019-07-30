e188352d3219fea675eb4eff117733148b9a7cd3
Diogo Silva <diogo@diogok.net>
2019-07-30 08:39:49 -0300

    Access log, auto tracing and routes on api.clj

--- src/server/api.clj
+++ src/server/api.clj
@@ -2,11 +2,15 @@
 (ns server.api
   (:require [opencensus-clojure.trace :refer [span add-tag]])
   (:require [taoensso.timbre :as log])
-  (:require [clojure.data.json :refer [read-str write-str]]))
+  (:require [clojure.data.json :refer [read-str write-str]])
+  
+  (:use [server.ws] :reload))
 
 (defn health 
   [_]
-  (span "health"
-    (log/spy
-      {:headers {"Content-Type" "application/json; charset=utf-8"}
-       :body (write-str {:ok true})})))
+    {:headers {"Content-Type" "application/json; charset=utf-8"}
+      :body (write-str {:ok true})})
+
+(def routes
+  [["/health" {:get {:handler health}}]
+       ["/ws" {:get {:handler ws}}]])
--- src/server/main.clj
+++ src/server/main.clj
@@ -2,33 +2,49 @@
   (:require [org.httpkit.server :refer [run-server]]
             [reitit.ring :as ring]
             [ring.util.response :refer [redirect response]]
-            [ring.middleware.reload :refer [wrap-reload]])
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.logger :refer [wrap-log-response]])
   (:require [iapetos.core :as prometheus]
-            [iapetos.collector.ring :as ring-prom])
-  (:require [taoensso.timbre :as log])
+            [iapetos.collector.ring :refer [wrap-metrics initialize]])
+  (:require [taoensso.timbre :as log]
+            [taoensso.timbre.appenders.3rd-party.logstash :refer [logstash-appender]])
   (:require [environ.core :refer [env]])
+  (:require [opencensus-clojure.ring.middleware :refer [wrap-tracing]])
+  (:require [clojure.string :as str])
 
-  (:use [server.ws] :reload)
-  (:use [server.api] :reload)
+  (:require [server.api :as api] :reload)
   
   (:gen-class))
 
 (defonce registry
   (-> (prometheus/collector-registry)
-      (ring-prom/initialize)))
+      (initialize)))
 
 (def routes
   (ring/ring-handler
-    (ring/router
-      [["/health" {:get {:handler health}}]
-       ["/ws" {:get {:handler ws}}]])))
+    (ring/router api/routes)))
+
+(defn ring-logger-fn
+  [{:keys [level throwable message]}]
+    (log/info level throwable message))
+
+(defn tracing-fn [req] 
+  (-> req :uri (str/replace #"/" "_")))
 
 (defn -main
   [& args] 
   #_(opencensus-clojure.reporting.jaeger/report "http://localhost:14268/api/traces" "my-service-name")
+
+  (if-let [logstash-host (env :logstash-host)]
+    (log/merge-config!
+      {:appenders
+        {:logstash (logstash-appender logstash-host (Integer/valueOf (env :logstash-port "5432")))}}))
+
   (log/spy
     (run-server 
-      ( -> #'routes
-          (ring-prom/wrap-metrics registry {:path "/metrics"})
+      (-> #'routes
+          (wrap-metrics registry {:path "/metrics"})
+          (wrap-log-response {:log-fn ring-logger-fn})
+          (wrap-tracing tracing-fn)
           (wrap-reload))
       {:port (Integer/valueOf (env :port "8080"))})))
