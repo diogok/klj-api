3e96b194795ee8503492c78932b31c662bdd7567
Diogo Silva <diogo@diogok.net>
2020-01-04 11:31:42 -0300

    Basic ring log

--- src/server/main.clj
+++ src/server/main.clj
@@ -1,9 +1,17 @@
 (ns server.main
   (:require [aleph.http.server :refer [start-server]]
             [reitit.ring :as ring])
-  (:require [clojure.tools.logging :as log])
+  (:require [clojure.tools.logging :as log]
+            [ring.logger :refer [wrap-log-request-params
+                                 wrap-log-request-start
+                                 wrap-log-response]])
   (:require [server.api :as api])
   (:gen-class))
+
+(defn ring-log-fn
+  "Send logs from ring to log lib"
+  [{:keys [level throwable message]}]
+  (log/log level throwable message))
 
 (def router
   (ring/router api/routes))
@@ -14,4 +22,9 @@
 (defn -main
   [& args]
   (log/info "Starting server")
-  (start-server routes {:port 8080}))
+  (start-server 
+   (-> routes
+       (wrap-log-response {:log-fn ring-log-fn})
+       (wrap-log-request-params {:log-fn ring-log-fn})
+       (wrap-log-request-start {:log-fn ring-log-fn})) 
+   {:port 8080}))
