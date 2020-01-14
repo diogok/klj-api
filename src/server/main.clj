699fb9d2536d9c664d6854d4a187499fe9558942
Diogo Silva <diogo@diogok.net>
2020-01-14 20:50:36 -0300

    Add tracing and close working native image

--- src/server/main.clj
+++ src/server/main.clj
@@ -11,7 +11,10 @@
                                  wrap-log-response]])
   
   (:require [iapetos.core :as prometheus]
+            #_[iapetos.collector.jvm :as prom-jvm]
             [iapetos.collector.ring :as prom-ring :refer [wrap-metrics]])
+
+  (:require [opencensus-clojure.ring.middleware :refer [wrap-tracing]])
   
   (:require [server.api :as api])
   
@@ -23,8 +26,10 @@
 (def routes
   (ring/ring-handler router))
 
+#_"TODO: how to add JVM metrics without braking native?"
 (defonce metric-registry
   (-> (prometheus/collector-registry)
+      #_(prom-jvm/initialize)
       (prom-ring/initialize)))
 
 (defn ring-log-fn
@@ -32,7 +37,7 @@
   [{:keys [level throwable message]}]
   (log/log level throwable message))
 
-(defn metric-path-fn
+(defn get-path
   [req]
   (->>
    req
@@ -40,12 +45,37 @@
    (r/match-by-path router)
    (:template)))
 
+(defn path-fn [req]
+  (or (get-path req) (:uri req)))
+
+(defn setup-tracing
+  []
+
+  (if (= "enabled" (env :tracing))
+    (opencensus-clojure.trace/configure-tracer {:probability 1.0})
+    (opencensus-clojure.trace/configure-tracer {:probability 0.0}))
+  
+  (when (= "enabled" (env :trace-log "disabled"))
+    (io.opencensus.exporter.trace.logging.LoggingTraceExporter/register))
+
+  (if-let [jaeger-host (env :jaeger-host)]
+    (io.opencensus.exporter.trace.jaeger.JaegerTraceExporter/createAndRegister
+     (str jaeger-host "/api/traces")
+     (env :app_name "myapp")))
+
+  (if-let [zipkin-host (env :zipkin-host)]
+    (io.opencensus.exporter.trace.zipkin.ZipkinTraceExporter/createAndRegister
+     (str zipkin-host "/api/traces")
+     (env :app_name "myapp"))))
+
 (defn -main
   [& args]
   (log/info "Starting server")
+  (setup-tracing)
   (start-server 
    (-> routes
-       (wrap-metrics metric-registry {:path "/metrics" :path-fn metric-path-fn})
+       (wrap-metrics metric-registry {:path "/metrics" :path-fn path-fn})
+       (wrap-tracing path-fn)
        (wrap-log-response {:log-fn ring-log-fn})
        (wrap-log-request-params {:log-fn ring-log-fn})
        (wrap-log-request-start {:log-fn ring-log-fn}))
